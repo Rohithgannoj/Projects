2b8054ece183b8dc73336f5064a5c4f32a148ac6
TL <TL@gmail.com>
2022-06-11 18:40:15 +0530

    Intitial structure OnlineBanking System

---
+++ OnlineBankingSystem/src/test/java/com/bankr/OnlineBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
